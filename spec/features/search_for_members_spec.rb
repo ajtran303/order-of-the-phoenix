a10de13839f42dfc328eb35baf538c0446a64e47
AJ Tran <ajtrandev@gmail.com>
2020-09-01 10:51:38 -0600

    Update view

--- spec/features/search_for_members_spec.rb
+++ spec/features/search_for_members_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe "Search for Gryffindor Order Members Spec" do
           expect(page).to have_selector("article[class*=member]", count: 21)
         end
         within(first(".member")) do
-          expect(page).to have_content("Name: Sirius Black")
+          expect(page).to have_content("Sirius Black")
           expect(page).to have_content("Role: n/a")
           expect(page).to have_content("House: Gryffindor")
           expect(page).to have_content("Patronus: n/a")
         end
         within(all(".member")[20]) do
-          expect(page).to have_content("Name: Ronald Weasley")
+          expect(page).to have_content("Ronald Weasley")
           expect(page).to have_content("Role: Student")
           expect(page).to have_content("House: Gryffindor")
           expect(page).to have_content("Patronus: Jack Russell Terrier")
