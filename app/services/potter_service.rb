ab643e8a1e6d0150d03548566654fa02fc8a45b8
AJ Tran <ajtrandev@gmail.com>
2020-09-01 09:47:43 -0600

    Add Faraday conn to PotterService

--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,2 +1,8 @@
 class PotterService
+  def conn
+    Faraday.new 'https://www.potterapi.com/v1/' do |f|
+      f.params['key'] = ENV['POTTER_API_KEY']
+      f.headers['Content-Type'] = 'application/json'
+    end
+  end
 end
