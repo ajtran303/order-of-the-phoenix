51b2e1ba15820445befc7b33d16a240af406a6ca
AJ Tran <ajtrandev@gmail.com>
2020-09-01 10:40:55 -0600

    Refactor potter_service

--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,6 +1,9 @@
 class PotterService
   def find_order_of_the_phoenix_members(hogwarts_house)
-    url = "https://www.potterapi.com/v1/characters?key=#{ENV['POTTER_API_KEY']}&orderOfThePhoenix=true&house=#{hogwarts_house}"
+    url = "https://www.potterapi.com/v1/characters" +
+          "?key=#{ENV['POTTER_API_KEY']}" +
+          "&orderOfThePhoenix=true" +
+          "&house=#{hogwarts_house}"
     response = Faraday.get url
     JSON.parse(response.body, symbolize_names: :true)
   end
