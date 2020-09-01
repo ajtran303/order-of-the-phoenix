d04372a40d73858ceab5429b4616545fb993c3b9
AJ Tran <ajtrandev@gmail.com>
2020-09-01 10:33:17 -0600

    Add method to phoenix facade

--- app/facades/phoenix_facade.rb
+++ app/facades/phoenix_facade.rb
@@ -1,5 +1,14 @@
 class PhoenixFacade
   def self.members_from_house(house)
-    # binding.pry
+    members = potter_service.find_order_of_the_phoenix_members(house)
+    @members = members.map do |member|
+      Character.new(member)
+    end
+  end
+
+  private
+
+  def self.potter_service
+    @potter_service ||= PotterService.new
   end
 end
